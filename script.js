e990fe084b357a14532aecdc542cff3462bec2f7
Imran Kabir <imran.kabir.k@gmail.com>
2025-01-12 17:19:13 +0500

    numbers auto commit on 2025-01-12 05:19:13 PM

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const container = document.querySelector('.container');
+let i = 0;
+const t = 100;
+let boxes = [];
+let timer = null;
 
 const get = (k, d) => JSON.parse(localStorage.getItem(`numbers-${k}`)) ?? d;
 const set = (k, v) => localStorage.setItem(`numbers-${k}`, JSON.stringify(v));
-
-const speak = n => {
-    const utter = new SpeechSynthesisUtterance(n);
-    utter.lang = 'en-US';
-    window.speechSynthesis.speak(utter);
-};
 
 const shuffle = numbers => {
     for (let i = numbers.length - 1; i > 0; i--) {
@@ -18,33 +15,59 @@ const shuffle = numbers => {
 };
 
 const display = numbers => {
-    for (const i of numbers) {
+    boxes = [];
+    const container = document.querySelector('.container');
+    for (const i in numbers) {
         const box = document.createElement('div');
         box.className = 'box';
-        box.textContent = i;
-        box.onclick = e => speak(i);
+        box.textContent = numbers[i];
+        box.onclick = e => start(i);
+        boxes.push(box);
         container.appendChild(box);
     }
 };
 
-document.querySelector('#shuffle').addEventListener('click', e => {
+const speak = (n, e) => {
+    const utter = new SpeechSynthesisUtterance(n);
+    utter.lang = 'en-US';
+    window.speechSynthesis.speak(utter);
+    e.classList.add('shake');
+    utter.onend = () => e.classList.remove('shake');
+};
+
+const start = n => {
+    i = n;
+    clearTimeout(timer);
+    play();
+};
+
+const play = e => {
+    let { total } = get('total', {total: t});
+    let { numbers } = get('numbers', {numbers: []});
+    speak(numbers[i], boxes[i]);
+    i = i < total - 1 ? ++i : 0;
+    setTimeout(play, 2500);
+};
+
+document.querySelector('#shuffle').onclick = e => {
     let { numbers } = get('numbers', {numbers: []});
     numbers = shuffle(numbers);
-    container.innerHTML = '';
+    document.querySelector('.container').innerHTML = '';
+    i = 0;
     display(numbers);
     set('numbers', {numbers});
-});
+};
 
 document.addEventListener('DOMContentLoaded', e => {
     let { numbers } = get('numbers', {numbers: []});
-
+    let { total } = get('total', {total: t});
     if (numbers.length === 0) {
-        for (let i = 1; i <= 100; i++) {
+        for (let i = 1; i <= total; i++) {
             numbers.push(i);
         }
         numbers = shuffle(numbers);
         set('numbers', {numbers});
+        set('total', {total});
     }
-
     display(numbers);
 });
